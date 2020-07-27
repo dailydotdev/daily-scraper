8d69d9e51f0ceed0a932ccfaea95dea88156870d
Ido Shamun <idoesh1@gmail.com>
2020-07-27 08:56:08 +0300

    refactor: increase acquire timeout

--- src/index.ts
+++ src/index.ts
@@ -82,7 +82,7 @@ const pptrPool = genericPool.createPool(
   {
     min: 1,
     max: 3,
-    acquireTimeoutMillis: 20000,
+    acquireTimeoutMillis: 60000,
     evictionRunIntervalMillis: 1000 * 60 * 10,
   },
 );
