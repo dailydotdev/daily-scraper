7bab98e6de3d331168648ed58291e264be999c00
Ido Shamun <idoesh1@gmail.com>
2020-07-27 09:11:04 +0300

    refactor: remove no-zygote flag

--- src/index.ts
+++ src/index.ts
@@ -71,7 +71,6 @@ const pptrPool = genericPool.createPool(
           '--disable-dev-shm-usage',
           '--disable-accelerated-2d-canvas',
           '--no-first-run',
-          '--no-zygote',
           '--single-process',
           '--disable-gpu',
         ],
