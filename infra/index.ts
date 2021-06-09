1e9b6239355f4e2abf8ad053be51ca38c18f6d86
Ido Shamun <idoesh1@gmail.com>
2021-06-09 14:44:48 +0300

    ci: use our pulumi-common lib and run db migrations in a job

--- infra/index.ts
+++ infra/index.ts
@@ -1,66 +1,44 @@
 import * as gcp from '@pulumi/gcp';
-import {
-  addIAMRolesToServiceAccount,
-  config,
-  createEnvVarsFromSecret,
-  infra,
-  location,
-} from './helpers';
 import { Output } from '@pulumi/pulumi';
+import {
+  CloudRunAccess,
+  createCloudRunService,
+  createEnvVarsFromSecret,
+  createServiceAccountAndGrantRoles,
+  imageTag,
+  infra,
+} from '@dailydotdev/pulumi-common';
 
 const name = 'scraper';
-
-const imageTag = config.require('tag');
 
 const vpcConnector = infra.getOutput(
   'serverlessVPC',
 ) as Output<gcp.vpcaccess.Connector>;
 
-const serviceAccount = new gcp.serviceaccount.Account(`${name}-sa`, {
-  accountId: `daily-${name}`,
-  displayName: `daily-${name}`,
-});
-
-addIAMRolesToServiceAccount(
+const { serviceAccount } = createServiceAccountAndGrantRoles(
+  `${name}-sa`,
   name,
+  `daily-${name}`,
   [
     { name: 'profiler', role: 'roles/cloudprofiler.agent' },
     { name: 'trace', role: 'roles/cloudtrace.agent' },
     { name: 'secret', role: 'roles/secretmanager.secretAccessor' },
   ],
-  serviceAccount,
 );
 
 const secrets = createEnvVarsFromSecret(name);
 
-const service = new gcp.cloudrun.Service(name, {
+const service = createCloudRunService(
   name,
-  location,
-  template: {
-    metadata: {
-      annotations: {
-        'autoscaling.knative.dev/maxScale': '20',
-        'run.googleapis.com/vpc-access-connector': vpcConnector.name,
-      },
-    },
-    spec: {
-      serviceAccountName: serviceAccount.email,
-      containers: [
-        {
-          image: `gcr.io/daily-ops/daily-${name}:${imageTag}`,
-          resources: { limits: { cpu: '1', memory: '512Mi' } },
-          envs: secrets,
-        },
-      ],
-    },
+  `gcr.io/daily-ops/daily-${name}:${imageTag}`,
+  secrets,
+  { cpu: '1', memory: '512Mi' },
+  vpcConnector,
+  serviceAccount,
+  {
+    access: CloudRunAccess.Public,
+    iamMemberName: `${name}-public`,
   },
-});
-
-new gcp.cloudrun.IamMember(`${name}-public`, {
-  service: service.name,
-  location,
-  role: 'roles/run.invoker',
-  member: 'allUsers',
-});
+);
 
 export const serviceUrl = service.statuses[0].url;
