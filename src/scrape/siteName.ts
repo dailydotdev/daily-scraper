90d2481dc2c84c49d3d2e78cad9fda6dbc2bd523
Ido Shamun <idoesh1@gmail.com>
2024-03-09 17:39:24 +0200

    feat: support youtube source scraping

--- src/scrape/siteName.ts
+++ src/scrape/siteName.ts
@@ -1,11 +1,24 @@
 import * as puppeteer from 'puppeteer';
+import { getPageType } from './common';
 
 export const scrapeSiteName = async (
   page: puppeteer.Page,
 ): Promise<string | null> => {
-  let [name] = await page.$$eval('meta[property="og:site_name"]', (el) =>
-    el.map((x): string => x.getAttribute('content')),
-  );
+  const pageType = getPageType(page);
+  let name: string;
+  if (pageType === 'youtube') {
+    await page.waitForSelector('#channel-header #channel-name #text', {
+      timeout: 1000,
+    });
+    [name] = await page.$$eval('#channel-header #channel-name #text', (el) =>
+      el.map((x): string => x.innerHTML),
+    );
+  }
+  if (!name) {
+    [name] = await page.$$eval('meta[property="og:site_name"]', (el) =>
+      el.map((x): string => x.getAttribute('content')),
+    );
+  }
   if (!name) {
     [name] = await page.$$eval('title', (el) =>
       el.map((x): string => x.innerHTML),
